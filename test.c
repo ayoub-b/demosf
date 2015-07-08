3ba045a78e25710656927ae08413f3965cc0deda
Ayoub Bacherki <ayoub.bacherki@gmail.com>
2015-07-08 11:30:35 +0200

    Test C code
    Test C code - used for demo

---
+++ test.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+void reverse(char * str) {
+    char *end = str;
+    char tmp;
+    if(str) {
+        while(*end) {
+//          printf("points to : %s \n", end);
+             ++end;
+        }
+        --end;
+        while (str < end){ 
+           tmp = *str; 
+           printf("tmp %d %c \n", tmp, tmp);
+           *str++ =*end; 
+           *end-- =tmp;
+       }
+    }
+    printf("final :%s\n", end);
+    //return str;
+}
+
+int main (int arc, char **argv ) {
+    if (arc > 0) {
+        reverse(argv[1]);
+        printf("%s", argv[1]);
+    }
+    return 1;
+}
